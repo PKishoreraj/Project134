be379da4c33d139c1f37d1ce818e2f232b64b231
PKishoreraj <113040359+PKishoreraj@users.noreply.github.com>
2023-06-03 19:34:13 +0530

    Add files via upload

--- p134main.js
+++ p134main.js
@@ -14,3 +14,22 @@ function setup(){
 function modelLoaded(){
     console.log('PoseNet Is Initialized!')
 }
+function gotPoses(results){
+    if(results.length>0){
+console.log(results);
+noseX=results[0].pose.nose.x;
+nosey=results[0].pose.nose.y;
+console.log("noseX="+noseX+"noseY="+noseY);
+leftWristx=results[0].pose.leftWrist.x;
+rightWristx=results[0].pose.rightWrist.x;
+difference=floor(leftWrist-rightWristx);
+console.log("lefthWrightx="+leftWristx+"rightWrightx="+rightWristx+"difference="+difference);
+}
+}
+function draw(){
+    background('#969A97');
+    document.getElementById("name_side").innerHTML="width and height of a square will be ="+difference+"px";
+    fill('#F90093');
+    stroke("#F90093");
+    spuare(noseX,noseY,difference);
+}
